ddc76364fe61c2b50dafdcfb16d4edada329b2c6
kenny <kenjlee09@gmail.com>
2025-11-04 21:17:52 -0800

    fiddling with neighbors and visited set

--- mazegen.js
+++ mazegen.js
@@ -6,28 +6,28 @@ function makeMaze(){
 
    };
    let node = [0,0];
-   console.log(node);
    let visited = new Set();
    graph = maze(graph, node, visited);
    return graph;
 }
 
 
-function findNeighbors(grid, node) {
+function findNeighbors(graph, node) {
    let neighbors = [];
    let y = 0;
    let x = 0;
-   for (let di of [-1, 0, 1]) {
+   console.log(node);
+   for (let di of [-1, 1]) {
       x = node[0] + di;
-      if (x >= 0 && x < SIZE && !(di == 0)) {
+      if (x >= 0 && x < SIZE) {
          neighbors.push([x, y]);
        }
      }
    x = 0;
-   for (let dj of [-1, 0, 1]) {
+   for (let dj of [-1, 1]) {
       y = node[1] + dj;
-      if (y >= 0 && y < SIZE && !(dj == 0)) {
-         neighbors.push(x,y);
+      if (y >= 0 && y < SIZE) {
+         neighbors.push([x,y]);
       }
    }
    return neighbors;
@@ -35,21 +35,29 @@ function findNeighbors(grid, node) {
 }
 
 
-function addEdge(node, neighbor) {
-   return [neighbor]
-   
+function addEdge(graph, node, neighbor) {
+   return [graph[node], neighbor];
 }
+
 
 function maze(graph, node, visited) {
    let neighbors = findNeighbors(graph, node);
+
+   console.log("bing");
+   console.log(visited);
+
+   console.log("bong");
+   console.log(neighbors);
    for (let neighbor of neighbors) {
       if (neighbor in visited) {
+         console.log("hello");
          continue;
       }
       visited.add(node);
 
-      console.log(graph);
-      graph[node] = addEdge(node, neighbors);
+
+      graph[node] = addEdge(graph, node, neighbor);
+      //graph[node] = addEdge(node, neighbors);
       maze(graph, neighbor, visited);
    }
    return graph;
