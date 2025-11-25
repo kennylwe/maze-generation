71ba1b5fad069c5dfc4de15fd2193f8972306255
Eric Lee <me@ericchanlee.com>
2025-11-24 16:03:17 -0800

    [TODO}

--- visualize.js
+++ visualize.js
@@ -24,4 +24,19 @@ export function finalMaze() {
    drawGrid(grid);
 }
 
+// 1. Write a function in visualize to draw the information on a square
+//    - f, g, h, parent
+//    - Test with fake nodes
+// 2. Run [astar]'s [init_graph] to get the "real" nodes
+//    - Don't worry about correct values
+//    - Set fake values for parents
+// 3. Color candidate / visited notes in some way
+//    - Visited be red
+//    - Candidates be green
+//    - Make up fake values for this to test
+// 4. Write tha actual Astar algorithm
+//    - At each time we "search" a candidate node, update drawing
+//    - Use this to debug!
+//    - https://www.youtube.com/watch?v=-L-WgKMFuhE&t=569s
+
 finalMaze()
